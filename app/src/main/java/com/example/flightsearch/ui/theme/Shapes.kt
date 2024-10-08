6fc118907b82e4c7df16ca198b50e20f2a1942b0
Robin Waginaire <robin.waginaire@gmail.com>
2024-10-08 16:10:00 +0200

    Improve search bar shape

--- app/src/main/java/com/example/flightsearch/ui/theme/Shapes.kt
+++ app/src/main/java/com/example/flightsearch/ui/theme/Shapes.kt
@@ -5,6 +5,6 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-//    small = RoundedCornerShape(50.dp),
-    medium = RoundedCornerShape(topEnd = 16.dp)
+    medium = RoundedCornerShape(topEnd = 16.dp),
+    extraSmall = RoundedCornerShape(25.dp)
 )
